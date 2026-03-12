e4cdb6d0c79d51f61ae7bec51ccbe379de91eb5e
AnavheobaAB <wisdomvolt@gmail.com>
2026-03-12 16:59:42 -0400

    trying to finish rpc integration almost

--- scripts/rpc/check_bitgert.sh
+++ scripts/rpc/check_bitgert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source "$(dirname "$0")/lib_rpc.sh"
-test_rpc "Bitgert" "evm" "" "" "" "https://rpc.icecreamswap.com"
+test_rpc "Bitgert" "evm" "" "" "" "https://rpc-bitgert.icecreamswap.com"
